136559a9a53fc644ffd141b4a5689a67873231ab
David Foster <dfosterj@pm.me>
2025-03-04 17:05:08 -0600

    full intel template

--- hardware/intel-hardware-custom.nix
+++ hardware/intel-hardware-custom.nix
@@ -3,6 +3,12 @@
 {
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
+  #  == Boot ==
+	# If you get an error like "your graphics device XXXX is not properly supported by i915 in this kernel version.
+	# force driver probe by i915.force_probe=7d45 module parameter
+	# or CONFIG_DRM_I915_FORCE_PROBE=7d45 configuration option.
+    #boot.kernelParams = [ "i915.force_probe=XXXX" ];
+
   #  == Hardware Custom  ==
 
   hardware = {
@@ -12,10 +18,11 @@
       enable = true;
       enable32Bit = true;
       extraPackages = with pkgs; [
-        vaapiIntel
-        vaapiVdpau
-        libvdpau-va-gl
-		mesa
+		 # vpl-gpu-rt         # for quick sync video (obs studio ffmpeg)
+		 # onevpl-intel-gpu   # for newer GPUs on NixOS <= 24.05
+		 # intel-media-sdk    # for older GPUs
+		 # vaapiIntel         # 6th Gen or Newer Specific Example
+		 # intel-media-driver # 6th Gen or Newer Specific Example
       ];
     };
   };
